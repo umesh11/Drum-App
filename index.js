db9203dc5873cd2214f8c7a8d447dc87b58c64cf
umesh11 <umesh11sh@gmail.com>
2021-12-01 12:50:25 +0530

    firstcommit

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 
-function playSound(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+function playMusic(event){
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
     if(!audio) return;
     audio.currentTime = 0; //will play again
     audio.play();
     key.classList.add('playing')
 }
     function removetransition(e){
-        if(e.propertyName !== 'transform')return; 
+        if(e.propertyName !== 'transform')
+        return; 
         this.classList.remove('playing')
       
     }
  const keys = document.querySelectorAll('.key');
  keys.forEach(key => key.addEventListener('transitionend', removetransition))
 
-window.addEventListener('keydown', playSound);
+window.addEventListener('keydown', playMusic);
